6e049cd46f54e71ba2352d61f824f72398b98405
nkbpp <alexbratchin@gmail.com>
2023-11-23 16:04:05 +0300

    fix

--- src/test/java/ru/bratchin/calculator/controller/CalculatorControllerTest.java
+++ src/test/java/ru/bratchin/calculator/controller/CalculatorControllerTest.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.bratchin.calculator.service.impl.CalculatorIntegerService;
-import ru.bratchin.calculator.service.impl.CalculatorMessageIntegerService;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
